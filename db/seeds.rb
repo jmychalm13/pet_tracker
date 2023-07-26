ca7ede5587c2ae18823efe2141124b8e1587f48e
“Jessica <jmychalm13@gmail.com>
2023-07-25 19:38:02 -0500

    add model with seeds for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Seeding started..."
+
+Pet.create(
+  name: "Mary Jane",
+  breed: "pitbull",
+  image_url: "https://lh3.googleusercontent.com/pw/AIL4fc8cNXF-I6_Llm-vEM2TnS7T3HGoIU0b7Nwd0-VPn-sqYbB03gN_AXgYuhkh4J5tvSHZPln1-_IadjMDytoS8svZj_cGmT5zwVMkce5LWaCDPBb3P2tq5YszVUQYQCtLtOZMLGh1nzFtwpzCnPWqr6NQs6EQegO1AxMAZqCmoWhyyaTVQ9JwGylnnV_tN19Ewtz86qa-vBnR1ZH3cm8eTB07d8E8Z1RVyQ3vVY24aw-oS_FWC_cMbLt1uef060dBQzVEWU_4YN-r3UyYwSTdj6KVqM9BbMU2NwfwZxO2k-9IWp1X4xJsa4KVP1cIljRjLYrsT6KcpvsDMRPYAOjeEPUDGDUlBZsXzxYfHVEGskzALujNbGnmNMIYvlFWMxuUXk9sjUqdBT-xECN4eIu459hSrtyi0cCDTS1XOHVLsR4yjrsZCuY78lyID690gQPcuvIAzqwZOVWZwS4Jd4KS39rmmyuS8_JAizs0mPzKX8uNAfx1URmROHgOvWsN2RjJcHaEuyChpgLWRzAs4s4PiLW-EDvUPFPha9f3PTDPXZKAeqyykZUmZ_7jI25t0hG-F9re1J7m7hTdSEU0TZE-kFTMGs5yROjavqRd1iVOl-1WfPIZwJaGblxtnI1j_XQbhQ3jb2RD2P26w1H3KDV3haqmoQQOE8M1zIDJblSjHl6F3_IwsBHinR7-wpWSSjgxSA_Sidyx1CYixEhBkFb7OS2UkUqqViVDIIYtvJ57LdNaw3A2khg2xpqS7uUKtWvVhHWnwCQ9QrI513WYiSPZcKFdMzqKolohdn6dg8m4JaAnjLliMN9Zop7wP4B4AUKyeNz4A5VfaebVVHjYIDKNYvh-UjMDB0q-DCKgpW6t49mSUw_jsnI-pWYA0MusZHROxYws-jDlrFpJEdUrsBPyDD2_Y_c=w994-h1324-s-no?authuser=0",
+)
+
+Pet.create(
+  name: "Nigel",
+  breed: "Great Pyranees",
+  image_url: "https://media.istockphoto.com/id/1225251586/photo/great-pyrenees-dog.jpg?s=612x612&w=0&k=20&c=ZU9gVVtOldXEoNHQg0HjoVTxW25u2JqPFIAiu-Hynu8=",
+)
+
+Pet.create(
+  name: "Grey",
+  breed: "a cat",
+  image_url: "https://media.gettyimages.com/id/913485798/photo/british-shorthair-cat.jpg?s=612x612&w=gi&k=20&c=aN-Qooq7WdiE61GDI3JuxhbAOkRxXCH5je2rE9rwUdo=",
+)
+
+puts "Seeding complete."
